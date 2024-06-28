9fb2a0e6fab9f3672cdd917b70a4bd10afa31d6e
Rodrigo Paiva <rodrigo_paiva_costa@hotmail.com>
2024-06-28 20:01:47 -0300

    Desafio do caique 3

---
+++ Aula1.py
@@ -0,0 +1,9 @@
+nome="Paula Martins"
+mês="Janeiro"
+valor_compra=5
+desconto=10
+cupom="PAULAE10"
+
+print("Ola,"+nome+".Em "+mês+"voce realizou uma compra no valor de R$"+valor_compra+"e ganhou um desconto de "+desconto+"% na sua proxima compra. Use o cupom"+cupom+".")
+
+
